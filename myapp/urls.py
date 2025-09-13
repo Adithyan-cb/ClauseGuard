e7bcb112ef2c9a28fb181c421da4d04f5ced0115
clauseguard-project <farhafazal42@gmail.com>
2025-09-12 23:43:10 -0700

    add user page

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import home,login,register,admin_dashboard
+from .views import home,login,register,admin_dashboard,user_dashboard,view_contracts,upload_contract,send_complaint,feedback
 urlpatterns = [
    path('',home,name='home'),
    path('login/',login,name="login"),
    path('register/',register,name="register"),
-   path('admin-dashboard/',admin_dashboard,name="admin-dashboard")
+   path('admin-dashboard/',admin_dashboard,name="admin-dashboard"),
+   path('user-dashboard/',user_dashboard,name='user-dashboard'),
+   path('view-contracts/',view_contracts,name='view-contract'),
+   path('upload-contract/',upload_contract,name='upload-contract'),
+   path('send-complaint/',send_complaint,name='send-complaint'),
+   path('send-feedback/',feedback,name='send-feedback'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -12,3 +12,18 @@ def register(request):
 
 def admin_dashboard(request):
     return render(request,'admin.html')
+
+def user_dashboard(request):
+    return render(request,'dashboard.html')
+
+def view_contracts(request):
+    return render(request,'viewContracts.html')
+
+def upload_contract(request):
+    return render(request,'uploadContract.html')
+
+def send_complaint(request):
+    return render(request,'complaint.html')
+
+def feedback(request):
+    return render(request,'feedback.html')
