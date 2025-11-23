9a71175184a885e31496a68d220ebd22dbefabad
Adithyan-cb <adithyancbadhi18@gmail.com>
2025-11-23 12:21:09 +0530

    change the name : "login" -> "login_user'

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home,login,register,admin_dashboard,user_dashboard,view_contracts,upload_contract,send_complaint,feedback
+from .views import home,login_user,register,admin_dashboard,user_dashboard,view_contracts,upload_contract,send_complaint,feedback
 urlpatterns = [
    path('',home,name='home'),
-   path('login/',login,name="login"),
+   path('login/',login_user,name="login"),
    path('register/',register,name="register"),
    path('admin-dashboard/',admin_dashboard,name="admin-dashboard"),
    path('user-dashboard/',user_dashboard,name='user-dashboard'),
